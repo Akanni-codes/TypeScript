58f456cd60f5343b92149b7d2586faa3d2a6bc6c
Akanni Silva <akanni029@gmail.com>
2025-08-27 15:29:40 -0300

    formatando codigo

--- Atividade1/questao1.ts
+++ Atividade1/questao1.ts
@@ -1,12 +1,11 @@
-
 import leia = require("readline-sync");
 
-let listaDeCores:Array<string> = new Array<string>()
+let listaDeCores: Array<string> = new Array<string>();
 for (let i = 0; i < 5; i++) {
-    let cor:string = leia.question('Insira a cor desejada: ')
-    listaDeCores.push(cor)
+  let cor: string = leia.question("Insira a cor desejada: ");
+  listaDeCores.push(cor);
 }
-console.log('Listar todas as cores:')
-console.log(listaDeCores)
-console.log('Ordenar as cores:')
-console.log(listaDeCores.sort())
+console.log("Listar todas as cores:");
+console.log(listaDeCores);
+console.log("Ordenar as cores:");
+console.log(listaDeCores.sort());
--- Atividade1/questao3.ts
+++ Atividade1/questao3.ts
@@ -1,11 +1,10 @@
 import readlinesync = require("readline-sync");
 
-let listaNum: Set<number> = new Set<number>()
+let listaNum: Set<number> = new Set<number>();
 
 do {
-    let num:number = readlinesync.questionInt('Insira o Valor desejado: ')
-    listaNum.add(num)
+  let num: number = readlinesync.questionInt("Insira o Valor desejado: ");
+  listaNum.add(num);
 } while (listaNum.size < 10);
 
-console.log(listaNum)
-
+console.log(listaNum);
